c8fbe4d1d8a5453aea9dc829a88a2841ec087deb
Sala 203 <sala203@ujaverianas.loc>
2025-04-11 14:34:37 -0500

    juan

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ with tab2:
            path=[px.Constant("PGN"),
                  "Nombre Sector",
                  "Tipo de gasto"],
-           values='Valor') 
+           values='Valor', color_continuous_scale = 'green') 
     st.plotly_chart(fig)
